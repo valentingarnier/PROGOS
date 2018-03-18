5f752ff2d34c23629eb474334a0fbd77ab323f3a
Valentin Garnier <valentingarnier@live.fr>
2018-03-18 17:06:57 +0100

    Adding serie 5 and other exercices

---
+++ OTHERS EXERCICES/exo1.c
@@ -0,0 +1,21 @@
+//
+//  exo1.c
+//  ProgOS
+//
+//  Created by Valentin Garnier  on 13.03.18.
+//  Copyright © 2018 Valentin Garnier . All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(void) {
+#define MAX_NAME 50
+    char nom_fichier[] = "";
+    FILE* ecriture = NULL;
+    
+    ecriture = fopen(nom_fichier, "wb");
+    
+    
+    
+    return 0;
+}
