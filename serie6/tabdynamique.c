bf14501d4a986c96a35a2884b3b28ba05af29ca6
Valentin Garnier <valentingarnier@live.fr>
2018-03-25 19:14:33 +0200

    tabupdate

--- serie6/tabdynamique.c
+++ serie6/tabdynamique.c
@@ -1,2 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+/* Created by Valentin Garnier - PROGOS Course */
+
+typedef int type_el;
+
+typedef struct {
+	size_t size;
+	size_t allocated;
+	type_el* content;
+} vector;
+
+//INITIALISATION
+
+#define VECTOR_PADDING 128
+vector* vector_construct(vector* v) {
+	vector* result = v;
+
+	if(result != NULL) {
+		result.content = calloc(VECTOR_PADDING, sizeof(type_el));
+		
+		if(result.content != NULL) {
+			result.size = 0;
+			result -> allocated = VECTOR_PADDING;
+		} else {
+			result = NULL;
+		}
+	}
+return result;
+}
