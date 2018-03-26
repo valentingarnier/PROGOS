d56a238d7dc6b82c2607fe2784e16efdc7cf6fcd
Valentin Garnier <valentingarnier@live.fr>
2018-03-26 08:13:13 +0200

    finished exercice1

--- serie6/tabdynamique.c
+++ serie6/tabdynamique.c
@@ -11,21 +11,74 @@ typedef struct {
 	type_el* content;
 } vector;
 
-//INITIALISATION
+int main(void) {
 
+printf("test \n");
+return 0;
+}
+
+//INITIALISATION
 #define VECTOR_PADDING 128
 vector* vector_construct(vector* v) {
 	vector* result = v;
 
 	if(result != NULL) {
-		result.content = calloc(VECTOR_PADDING, sizeof(type_el));
+		(*result).content = calloc(VECTOR_PADDING, sizeof(type_el));
 		
-		if(result.content != NULL) {
-			result.size = 0;
-			result -> allocated = VECTOR_PADDING;
+		if((*result).content != NULL) {
+			(*result).size = 0;
+			(*result).allocated = VECTOR_PADDING;
 		} else {
 			result = NULL;
 		}
 	}
 return result;
 }
+
+//DESTRUCTION
+void vector_destruct(vector* v) {
+	if((v != NULL) && (v->content != NULL)) {
+		free((*v).content);
+		(*v).content = NULL;
+		(*v).size = 0;
+		(*v).allocated = 0;
+	}
+}
+
+//AJOUT D'UN ELEMENT
+size_t add_element(vector* vect, type_el element) {
+ 	if(vect != NULL) {
+ 	(*vect).content[(*vect).size] = element;
+ 	++((*vect).size);
+
+ 	return (*vect).size;
+ }
+ else { return 0; }
+}
+
+type_el change_value(vector* vect, size_t index, type_el newValue) {
+	if(vect != NULL) {
+		(*vect).content[index] = newValue;
+		return newValue;
+	} else {
+		return 0;
+	}	
+}
+
+type_el read_value(vector* vect, size_t index) {
+	if(vect != NULL) {
+		type_el result = (*vect).content[index];
+		return result;
+	}
+	else {
+		return 0;
+	}
+}
+
+
+
+
+
+
+
+
