235a5f99aa3e38cb429b03cd47813340d0c4079c
Valentin Garnier <valentingarnier@live.fr>
2018-04-09 09:24:01 +0200

    serie7

--- serie7/lettre.c
+++ serie7/lettre.c
@@ -2,23 +2,50 @@
 #include <stdio.h>
 #include <string.h>
 
+
+typedef enum Genre Genre;
+enum Genre { M, F };
+
 void genereLettre();
 
 int main(void) {
-	genereLettre();
+	char genre[] = "chère";
+	char destinataire[] = "Mireille";
+	char sujet[] = "votre cours";
+	char politesse[] = "Amicalement";
+	char auteur[] = "John";
+	int jour = 18;
+	int mois = 12;
+
+	genereLettre(genre, destinataire, sujet, jour, mois, politesse, auteur);
+
+	/*genre[] = "cher";
+	destinataire[] = "John";
+	sujet[] = "votre demande de rendez-vous.";
+	jour[] = 16;
+	politesse[] = "Sincèrement";
+	auteur[] = "Mireille"; */
+
+	printf("\n");
+	printf("\n");
+	fflush(stdout);
+	genereLettre(genre, destinataire, sujet, jour, mois, politesse, auteur);
+
+	printf("\n");
+	fflush(stdout);
 
 	return 0;
 }
 
-void genereLettre() {
-	printf("Bonjour chère Mireille,\n");
+void genereLettre(char genre[], char destinataire[], char sujet[], int jour, int mois, char politesse[], char auteur[]) {
+	printf("Bonjour %s %s,\n", genre, destinataire);
 	fflush(stdout);
-	printf("Je vous écris à propos de votre cours.\n");
+	printf("Je vous écris à propos de %s.\n", sujet);
 	fflush(stdout);
-	printf("Il faudrait que nous nous voyons le 18/12 pour en discuter.\n");
+	printf("Il faudrait que nous nous voyons le %d/%d pour en discuter.\n", jour, mois);
 	fflush(stdout);
 	printf("Donnez-moi vite de vos nouvelles !\n");
 	fflush(stdout);
-	printf("Amicalement, John.\n");
+	printf("%s, %s.\n", politesse, auteur);
 	fflush(stdout);
 }
---
+++ serie7/token.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int nextToken(char const *, int*, int*);
+
+int nextToken(char const * str, int* from, int* len) {
+
+}
+
+int main(void) {
+	
+}
